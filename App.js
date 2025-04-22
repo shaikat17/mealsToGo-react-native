64b30eaf6232ffdde7081e9c103ccc9007c8836e
shaikat17 <shaikatpal56@gmail.com>
2025-04-22 14:48:10 +0600

    Added safearea for android.

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Platform, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import Constants from "expo-constants";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={{ backgroundColor: "blue" }}>
+          <Text>Search</Text>
+        </View>
+        <View style={{ backgroundColor: "green", flex: 1 }}>
+          <Text>Search</Text>
+        </View>
+      </SafeAreaView>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === "android" ? Constants.statusBarHeight : 0,
   },
 });
